23beaa040e38786d2eaff91b96ff82c8398f501e
Fati-810 <49760018+Fati-810@users.noreply.github.com>
2025-03-28 19:09:01 +0500

    Update app.js

--- app.js
+++ app.js
@@ -83,6 +83,8 @@ app.get("/posts/:postId", function (req, res) {
         console.log(err);
     });
 });
-app.listen(3000, function () {
-    console.log("Server started on port 3000");
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("Server started on port " + port);
 });
